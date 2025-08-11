48f143ef5f645b2dbde515dde684b670d1d760f1
ArmaanS <armaanshaikh0706@gmail.com>
2025-08-12 00:34:15 +0530

    done

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { LoadingService } from './app/services/loading.service';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [...(appConfig.providers || []), LoadingService],
-}).catch((err) => console.error(err));
+  providers: [
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ]
+}).catch(err => console.error(err));
