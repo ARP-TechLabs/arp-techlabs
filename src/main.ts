5658c45a5ee1edaf71247cd32959a9fdc6878ba9
ArmaanS <armaanshaikh0706@gmail.com>
2025-08-13 00:13:40 +0530

    feat: add Vercel analytics and speed insights integration

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { LoadingService } from './app/services/loading.service';
-
+import { injectSpeedInsights } from '@vercel/speed-insights';
+ 
+injectSpeedInsights();
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [...(appConfig.providers || []), LoadingService],
